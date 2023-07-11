4f23a50952f9f4a5cbeff4b1f5497d0b30148d12
kt <cjtakhar@gmail.com>
2023-07-10 22:08:26 -0700

    update

--- src/components/budget.jsx
+++ src/components/budget.jsx
@@ -4,7 +4,6 @@ import './styles/budget.css';
 const BudgetForm = () => {
   const [budget, setBudget] = useState('');
   const [billAmount, setBillAmount] = useState('');
-  const [bills, setBills] = useState([]);
   const [savings, setSavings] = useState(0);
   const [investment, setInvestment] = useState(0);
 
@@ -16,20 +15,8 @@ const BudgetForm = () => {
     setBillAmount(parseFloat(event.target.value));
   };
 
-  const handleAddBill = (event) => {
-    event.preventDefault();
-    if (billAmount) {
-      const newBill = {
-        amount: billAmount,
-      };
-      setBills([...bills, newBill]);
-      setBillAmount(''); // Reset billAmount after adding the bill
-    }
-  };
-
   const calculateSavingsAndInvestment = () => {
-    const totalBills = bills.reduce((sum, bill) => sum + bill.amount, 0);
-    const remainingBudget = budget - totalBills;
+    const remainingBudget = budget - billAmount;
     const savingsAmount = remainingBudget * (3 / 4);
     const investmentAmount = remainingBudget * (1 / 4);
     setSavings(savingsAmount);
@@ -42,7 +29,7 @@ const BudgetForm = () => {
 
   return (
     <div className="budget-container">
-      <h2>Budget and Bills</h2>
+      <h2>Monthly</h2>
       <form className="budget-form">
         <label>
           <input type="number" value={budget} onChange={handleBudgetChange} placeholder="budget" />
@@ -53,18 +40,17 @@ const BudgetForm = () => {
         </label>
         <br />
         <br />
-        <button className="bill-btn" onClick={handleAddBill}>Add Bill</button>
       </form>
 
       <button className="budget-calculate-btn" onClick={calculateSavingsAndInvestment}>
         Calculate Savings and Investment
       </button>
       <div className="form-output">
-      <h3>Budget: {budget}</h3>
-      <h3>Bills: {bills.map((bill) => `$${bill.amount}`).join(', ')}</h3>
+        <h3>Budget: {budget}</h3>
+        <h3>Bills: {billAmount}</h3>
 
-      <h3>Savings: ${savings}</h3>
-      <h3>Investment: ${investment}</h3>
+        <h3>Savings: ${savings}</h3>
+        <h3>Investment: ${investment}</h3>
       </div>
       <div className="year">
         <h3>Annual Savings: ${oneYearSavings}</h3>
@@ -76,5 +62,3 @@ const BudgetForm = () => {
 };
 
 export default BudgetForm;
-
-
